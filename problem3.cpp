a332b427452f0dc3871bf16424704468a6536108
nodirbek2209 <147589242+nodirbek2209@users.noreply.github.com>
2024-04-04 15:35:49 +0300

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
-#include <iomanip>
-using namespace std;
-int factorial(int a,int b){
-    if(a==b){
-        return b;
-    }else{
-        return a*factorial(a-1,b);
-    }
 
+using namespace std;
+
+// Recursive function to compute binomial coefficient
+int binomialCoefficient(int n, int k) {
+    // Base cases: if k equals 0 or n, return 1
+    if (k == 0 || k == n) {
+        return 1;
+    }
+    // Recursive case: compute binomial coefficient using the formula
+    return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
 }
+
 int main() {
-    int a,b;
-    cin>>a>>b;
-    cout<<factorial(a,b+1)/ factorial(a-b,1);
+    int n, k;
+    cin >> n >> k;
+
+    // Check if k is greater than n
+    if (k > n) {
+        cout << "Error: k cannot be greater than n!" << endl;
+    } else {
+        
+        cout<< binomialCoefficient(n, k) << endl;
+    }
 
     return 0;
 }
