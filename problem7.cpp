d7b85e28a83722f9c88e0e294d1f058ea7b5ae1c
nodirbek2209 <147589242+nodirbek2209@users.noreply.github.com>
2024-04-04 15:39:44 +0300

    Update problem7.cpp

--- problem7.cpp
+++ problem7.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-string substring_recursive(const string& str, int i, int j) {
-    // Base case: if i and j are equal, return the character at position i
-    if (i == j) {
-        return string(1, str[i]);
-    } else {
-        // Recursive case: append the character at position i and call the function with incremented i
-        return str[i] + substring_recursive(str, i + 1, j);
+string substring(const string& str, int i, int j) {
+    // Base case: if i is greater than j or j is out of range, return an empty string
+    if (i > j || j >= str.length()) {
+        return "";
     }
+    // Base case: if i is 0, return the substring up to position j
+    if (i == 0) {
+        return str.substr(0, j + 1);
+    }
+    // Recursive case: return substring excluding the first character
+    return substring(str.substr(1), i - 1, j - 1);
 }
 
 int main() {
-    string input_string;
-    cin>>input_string;
-    int start_index;
-    int end_index;
-    cin>>start_index>>end_index;
-    cout << substring_recursive(input_string, start_index, end_index) << endl; 
-
-    
-
+    string s;
+    cin>>s;
+    int a,b;
+    cin>>a>>b;
+    cout<<substring(s,a,b);
     return 0;
 }
